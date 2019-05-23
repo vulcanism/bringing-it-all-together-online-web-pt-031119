ec86481ad3ec46de376271aa59705f62df618a8d
Savannah C <sgcorrell@gmail.com>
2019-05-23 16:24:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,7 +69,7 @@ class Dog
     
     if !dog.empty?
       dog_details = dog[0]
-      dog = Dog.new(dog_details[0], dog_details[1], dog_details[2])
+      dog = Dog.new(id: dog_details[0], name: dog_details[1], breed: dog_details[2])
     else
       dog.create(name: name, breed: breed)
     end
