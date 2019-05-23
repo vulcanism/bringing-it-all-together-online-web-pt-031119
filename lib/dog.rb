fe7a4f1018e2d08be0c8fdb6931467659c4b520d
Savannah C <sgcorrell@gmail.com>
2019-05-23 16:09:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,6 +29,7 @@ class Dog
           VALUES (?, ?)"
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
   end
   
 end
