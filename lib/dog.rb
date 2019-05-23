ec401ac6dc97ab1c000dc00f296ba3ae58158290
Savannah C <sgcorrell@gmail.com>
2019-05-23 16:03:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,14 @@ class Dog
     @breed = breed
   end
   
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+      );
+    SQL
+  end
+  
 end
