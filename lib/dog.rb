1720f116e5c1d3c34c863b47812e983f8b614a6c
Savannah C <sgcorrell@gmail.com>
2019-05-23 16:11:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,9 @@ class Dog
   end
   
   def self.create(name:, breed:)
-    new_dog = self.new(name, breed)
+    new_dog = self.new(name: name, breed: breed)
+    new_dog.save
+    new_dog
   end
   
 end
