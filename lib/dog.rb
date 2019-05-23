6cd98cc625974b5e3d3c35bb224aec90058fbf8d
Savannah C <sgcorrell@gmail.com>
2019-05-23 16:26:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,13 @@ class Dog
   end
   
   def update
-    
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
 end
